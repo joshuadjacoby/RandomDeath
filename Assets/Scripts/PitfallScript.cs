8c0735fb6e826c041516c68146418ae47a82b0b5
Robert Skelton <robertmskelton@gmail.com>
2015-04-11 13:38:13 -0700

    basic pitfall prints when triggered

---
+++ Assets/Scripts/PitfallScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PitfallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// RS: upon entering trigger, send player to another room
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.tag == "Player")
+			Debug.Log ("im falling!");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
