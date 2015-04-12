1926b22daed24a0c21119a2148bf1ef6d774a51f
buckslice <buckslice@gmail.com>
2015-04-11 17:03:41 -0700

    added start of 3D level rendering system

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,30 +5,58 @@ using System.Collections.Generic;
 public class LevelLoader : MonoBehaviour {
 
     public int currentLevel = 0;
-    public int[][] tiles;
+    public int[,] tiles;
     public int numberOfLevels;
+
+    public Texture2D[] textures;
+    public Texture2D atlas;
+    public Rect[] rects;
+
     private GameObject player;
     private Vector2 playerStart;
 
-	// Use this for initialization
-	void Start () {
-	    // load all levels
+    private Mesh mesh;
+
+    private List<int> tris = new List<int>();
+    private List<Vector3> verts = new List<Vector3>();
+    private List<Vector2> uvs = new List<Vector2>();
+    private int triNum = 0;
+
+    // ids that correspond to the .png level reader
+    private const int GROUND = 0;
+    private const int WALL = 1;
+    private const int CRATE = 2;
+    private const int SPIKES = 3;
+
+    // Use this for initialization
+    void Start() {
+        // load all levels
         player = GameObject.Find("Player");
         playerStart = player.transform.position;
 
+        atlas = new Texture2D(1024, 1024);
+        atlas.filterMode = FilterMode.Point;
+        rects = atlas.PackTextures(textures, 0, 1024);
+
+        Material mat = (Material)Resources.Load("sprites/level");
+        mat.SetTexture(0, atlas);
+
+        GetComponent<MeshRenderer>().material = mat;
+
         LoadLevel();
 
-        
-	}
+
+    }
 
     private void LoadLevel() {
-        
+
         Texture2D tex = (Texture2D)Resources.Load("levels/level1");
         Color32[] colors = tex.GetPixels32();
 
         Dictionary<Color32, int> table = new Dictionary<Color32, int>();
         int n = 0;
         int width = tex.width;
+        int height = tex.height - 1;
         for (int i = colors.Length - tex.width; true; i++) {
             Color32 color = colors[i];
             if (!table.ContainsKey(color)) {
@@ -37,67 +65,160 @@ public class LevelLoader : MonoBehaviour {
                 break;
             }
         }
-        
-        int[] tiles = new int[colors.Length - tex.width];
+
+        int[] tiles1D = new int[colors.Length - tex.width];
         for (int i = 0; i < colors.Length - tex.width; i++) {
-            tiles[i] = table[colors[i]];
+            tiles1D[i] = table[colors[i]];
         }
 
+        tiles = new int[width, height];
 
-        Sprite ground = Resources.Load<Sprite>("sprites/ground");
-        Sprite wall = Resources.Load<Sprite>("sprites/wall");
-        Sprite spikes = Resources.Load<Sprite>("sprites/spikes");
+        // turn into 2D array
+        for (int i = 0; i < tiles1D.Length; i++) {
+            tiles[i % width, i / height] = tiles1D[i];
+        }
 
-        float tileSize = 32f / 100f;
+        tris.Clear();
+        verts.Clear();
+        uvs.Clear();
+        triNum = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
 
-        GameObject tilesGameObject = new GameObject("tiles");
-        
-        for (int i = 0; i < tiles.Length; i++) {
-            GameObject go = new GameObject();
+                int h = getHeight(x, y); //gets the height of this tile
 
-            go.transform.parent = tilesGameObject.transform;
-            
-            SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+                // add 2 triangles for this tile
+                verts.Add(new Vector3(x, h, y));
+                verts.Add(new Vector3(x, h, y + 1));
+                verts.Add(new Vector3(x + 1, h, y + 1));
+                verts.Add(new Vector3(x + 1, h, y + 1));
+                verts.Add(new Vector3(x + 1, h, y));
+                verts.Add(new Vector3(x, h, y));
 
+                addUvsTris(tiles[x, y]);
 
-            go.transform.position = new Vector2((i % width) * tileSize, (i / width) * tileSize);
-            switch (tiles[i]) {
-                case 0:
-                    sr.sprite = ground;
-                    go.name = "ground";
-                    break;
-                case 1:
-                    sr.sprite = wall;
-                    go.name = "wall";
-                    go.AddComponent<BoxCollider2D>();
-                    go.AddComponent<Rigidbody2D>().isKinematic = true;
-                    break;
-                case 2:
-                    sr.sprite = spikes;
-                    go.name = "spikes";
-                    BoxCollider2D bc2 = go.AddComponent<BoxCollider2D>();
-                    bc2.isTrigger = true;
-                    bc2.size = bc2.size * .5f;
-                    go.AddComponent<Rigidbody2D>().isKinematic = true;
-                    go.AddComponent<SpikeScript>();
-                    break;
-                default:
-                    break;
+                // if this tile is high and neighbors are low then add walls down your sides
+                if (h == 1) {
+                    if (getHeight(x + 1, y) == 0) { // right neighbor
+                        verts.Add(new Vector3(x + 1, 0, y));
+                        verts.Add(new Vector3(x + 1, h, y));
+                        verts.Add(new Vector3(x + 1, h, y + 1));
+                        verts.Add(new Vector3(x + 1, h, y + 1));
+                        verts.Add(new Vector3(x + 1, 0, y + 1));
+                        verts.Add(new Vector3(x + 1, 0, y));
+
+                        addUvsTris(tiles[x, y]);
+                    }
+
+                    if (getHeight(x - 1, y) == 0) { // left neighbor
+                        verts.Add(new Vector3(x, 0, y + 1));
+                        verts.Add(new Vector3(x, h, y + 1));
+                        verts.Add(new Vector3(x, h, y));
+                        verts.Add(new Vector3(x, h, y));
+                        verts.Add(new Vector3(x, 0, y));
+                        verts.Add(new Vector3(x, 0, y + 1));
+
+                        addUvsTris(tiles[x, y]);
+                    }
+
+                    if (getHeight(x, y + 1) == 0) { // top neighbor
+                        verts.Add(new Vector3(x + 1, 0, y + 1));
+                        verts.Add(new Vector3(x + 1, h, y + 1));
+                        verts.Add(new Vector3(x, h, y + 1));
+                        verts.Add(new Vector3(x, h, y + 1));
+                        verts.Add(new Vector3(x, 0, y + 1));
+                        verts.Add(new Vector3(x + 1, 0, y + 1));
+
+                        addUvsTris(tiles[x, y]);
+                    }
+
+                    if (getHeight(x, y - 1) == 0) { // bottom neighbor
+                        verts.Add(new Vector3(x, 0, y));
+                        verts.Add(new Vector3(x, h, y));
+                        verts.Add(new Vector3(x + 1, h, y));
+                        verts.Add(new Vector3(x + 1, h, y));
+                        verts.Add(new Vector3(x + 1, 0, y));
+                        verts.Add(new Vector3(x, 0, y));
+
+                        addUvsTris(tiles[x, y]);
+                    }
+                }
+
+                // this is where you can spawn certain prefabs based on what tile you are
+                switch (tiles[x, y]) {
+
+                    case 0:
+
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+
+                }
+
             }
         }
 
-        // make tiles twice as big
-        tilesGameObject.transform.localScale = new Vector3(1/tileSize, 1/tileSize, 1/tileSize);
+        // build mesh from lists
+        mesh = new Mesh();
+        mesh.vertices = verts.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.triangles = tris.ToArray();
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void addUvsTris(int index) {
+        if (index == 1) {
+            index = Random.value < .2 ? 4 : 1;
+        }
+        
+        Rect r = rects[index];
+
+        uvs.Add(new Vector2(r.xMin, r.yMax));
+        uvs.Add(new Vector2(r.xMin, r.yMin));
+        uvs.Add(new Vector2(r.xMax, r.yMin));
+        uvs.Add(new Vector2(r.xMax, r.yMin));
+        uvs.Add(new Vector2(r.xMax, r.yMax));
+        uvs.Add(new Vector2(r.xMin, r.yMax));
+
+        tris.Add(triNum++);
+        tris.Add(triNum++);
+        tris.Add(triNum++);
+        tris.Add(triNum++);
+        tris.Add(triNum++);
+        tris.Add(triNum++);
+    }
+
+
+    private int getHeight(int x, int y) {
+        if (x < 0 || x >= tiles.GetLength(0) || y < 0 || y >= tiles.GetLength(1)) {
+            return 1;
+        }
+        switch (tiles[x, y]) {
+            case WALL:
+            case CRATE:
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetKey(KeyCode.R)) {
             player.transform.position = playerStart;
             PlayerScript.health = 1;
             player.SetActive(true);
 
         }
-	}
+    }
 }
