f03eb190ea8d2e9dd580af41ce6076d13f78361e
PremNishanth <pkothand@uci.edu>
2015-04-11 23:45:56 -0700

    Level Change

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelChanger : MonoBehaviour {
+
+	private LevelLoader l =  new LevelLoader();
+
+	private string level;
+
+	void Start()
+	{
+		//l = GameObject.FindGameObjectWithTag ("Level");
+	}
+
+	void Update()
+	{
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("HEY");
+			//level = "levels/level2";
+			l.LoadLevel("levels/level2");
+
+		}
+	}
+
+	
+}
+
+
