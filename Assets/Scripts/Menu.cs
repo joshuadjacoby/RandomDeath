736d28e1d83bffc3ec029b77bae2b0dc36a959ff
PremNishanth <pkothand@uci.edu>
2015-04-12 21:20:07 -0700

    Added Menu Scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown ("Fire3")) {
+
+			Application.LoadLevel("Main");
+		}
+	}
+
+    void OnMouseDown() {
+     if (this.name == "PlayBT")
+     {
+   		Application.LoadLevel("Main");
+     }
+    }
+}
