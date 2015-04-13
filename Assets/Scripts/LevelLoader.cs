0e1372bd8db8282365eb4789a4b9e4d79c500c8e
Robert Skelton <robertmskelton@gmail.com>
2015-04-12 22:18:11 -0700

    levels

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class LevelLoader : MonoBehaviour {
 
-    private int currentLevel = 20;
+    private int currentLevel = 7;
     public int[,] tiles;
     private int numberOfLevels;
 
